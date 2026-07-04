96b58362e7f026a7865cfa8dde154a936c993b2d
Parham Abbasi <prmbas@gmail.com>
2026-07-04 23:48:05 +0330

    signle status added and error fixed

--- api/status.go
+++ api/status.go
@@ -9,9 +9,26 @@ func (h *Handler) Status(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 	}
-	jsonData, err := json.MarshalIndent(h.Core.GetAll(), "", " ")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	serviceName := r.URL.Query().Get("service")
+	var jsonData []byte
+	var err error
+	if serviceName != "" {
+		val, ok := h.Core.Get(serviceName)
+		if !ok {
+			http.Error(w, "Service Not Found", http.StatusNotFound)
+			return
+		}
+		jsonData, err = json.MarshalIndent(val, "", " ")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	} else {
+		jsonData, err = json.MarshalIndent(h.Core.GetAll(), "", " ")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(jsonData)
--- api/status_test.go
+++ api/status_test.go
@@ -45,3 +45,25 @@ func TestStatus(t *testing.T) {
 	}
 	t.Logf("%s", body)
 }
+
+func TestStatusSignleService(t *testing.T) {
+	h := NewHandler()
+	// mock data
+	serviceName := "mock_single_service"
+	url := "/status?service=" + serviceName
+	// mock request and writer
+	h.Core.Add(serviceName)
+	w, r := responseAndRequestBuild(
+		http.MethodGet,
+		url,
+		nil,
+	)
+	// handle function
+	h.Status(w, r)
+	defer w.Result().Body.Close()
+	body, err := io.ReadAll(w.Result().Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	t.Logf("%s", body)
+}
