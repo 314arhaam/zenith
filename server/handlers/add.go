099a69e4302b4b11e31942e94ac710acea48e11e
Parham Abbasi <prmbas@gmail.com>
2026-07-05 22:07:02 +0330

    comments added

--- server/handlers/add.go
+++ server/handlers/add.go
@@ -7,20 +7,26 @@ import (
 )
 
 func (h *Handler) Add(w http.ResponseWriter, r *http.Request) {
+	// check if method ok
+	// Post -> ok
+	// other -> http.StatusMethodNotAllowed -> return
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
+	// read request data
 	defer r.Body.Close()
 	var rs data.RequestPayload
 	if err := json.NewDecoder(r.Body).Decode(&rs); err != nil {
 		http.Error(w, "Error in request decode "+err.Error(), http.StatusInternalServerError)
 		return
 	}
+	// validate request, check if it has the key
 	if rs.ServiceName == "" {
 		http.Error(w, "Not Data Provided", http.StatusBadRequest)
 		return
 	}
+	// core logic
 	h.Core.Add(rs.ServiceName)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
