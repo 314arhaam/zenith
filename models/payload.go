21478727312b8fc17513d93fe7371bc710ae1acb
Parham Abbasi <prmbas@gmail.com>
2026-07-06 20:37:51 +0330

    to json added

--- models/payload.go
+++ models/payload.go
@@ -1,5 +1,11 @@
 package data
 
+import "encoding/json"
+
 type RequestPayload struct {
 	ServiceName string `json:"service_name"`
+}
+
+func (r *RequestPayload) ToJson() ([]byte, error) {
+	return json.Marshal(r)
 }
