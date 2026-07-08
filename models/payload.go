61440e455467c5130b1967af7c85af81c1a34584
Parham Abbasi <prmbas@gmail.com>
2026-07-08 21:46:29 +0330

    struct and interface for response and request

--- models/payload.go
+++ models/payload.go
@@ -1,11 +1,5 @@
 package data
 
-import "encoding/json"
-
 type RequestPayload struct {
 	ServiceName string `json:"service_name"`
-}
-
-func (r *RequestPayload) ToJson() ([]byte, error) {
-	return json.Marshal(r)
 }
