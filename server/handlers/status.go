c5c0af237f3e7046318555ab4b256280f7f87b8a
Parham Abbasi <prmbas@gmail.com>
2026-07-09 05:14:14 +0330

    new architecture

--- server/handlers/status.go
+++ server/handlers/status.go
@@ -2,9 +2,23 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
-	"strings"
+	"zenith/core"
 )
+
+func (h *Handler) status(serviceName string) (map[string]core.Service, error) {
+	if serviceName != "" {
+		val, ok := h.Core.Get(serviceName)
+		if !ok {
+			return nil, fmt.Errorf("Service not found: %s", serviceName)
+		}
+		return map[string]core.Service{serviceName: val}, nil
+	} else {
+		val := h.Core.GetAll()
+		return val, nil
+	}
+}
 
 func (h *Handler) Status(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
@@ -13,23 +27,14 @@ func (h *Handler) Status(w http.ResponseWriter, r *http.Request) {
 	}
 	serviceName := r.URL.Query().Get("service")
 	w.Header().Set("Content-Type", "application/json")
-	if serviceName != "" {
-		val, ok := h.Core.Get(serviceName)
-		if !ok {
-			msg := strings.Join([]string{"Service Not Found: ", serviceName}, "")
-			http.Error(w, msg, http.StatusNotFound)
-			return
-		}
-		w.WriteHeader(http.StatusOK)
-		if err := json.NewEncoder(w).Encode(val); err != nil {
-			http.Error(w, "Error in encoding", http.StatusInternalServerError)
-			return
-		}
-	} else {
-		w.WriteHeader(http.StatusOK)
-		if err := json.NewEncoder(w).Encode(h.Core.GetAll()); err != nil {
-			http.Error(w, "Error in encoding", http.StatusInternalServerError)
-			return
-		}
+	val, err := h.status(serviceName)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(val); err != nil {
+		http.Error(w, "Error in encoding", http.StatusInternalServerError)
+		return
 	}
 }
